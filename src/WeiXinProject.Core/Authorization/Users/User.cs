bed2cbd34109ae29d39fa1248862e7f82b518a53
15261883163 <weiyunchuan@163.com>
2020-04-21 15:54:00 +0800

    user新增字段提交

--- src/WeiXinProject.Core/Authorization/Users/User.cs
+++ src/WeiXinProject.Core/Authorization/Users/User.cs
@@ -30,5 +30,89 @@ namespace WeiXinProject.Authorization.Users
 
             return user;
         }
+        ///<Summary>
+        /// GroupID 分组：机构型,客户型
+        ///</Summary>
+        [Display(Name = "分组：机构型,客户型")]
+        public int GroupID { get; set; }
+
+        ///<Summary>
+        /// Avatar 头像
+        ///</Summary>
+        [Display(Name = "头像")]
+        public string Avatar { get; set; }
+
+        ///<Summary>
+        /// Sex 性别
+        ///</Summary>
+        [Display(Name = "性别")]
+        public int Sex { get; set; }
+
+        ///<Summary>
+        /// Birthday 生日
+        ///</Summary>
+        [Display(Name = "生日")]
+        public DateTime Birthday { get; set; }
+
+        ///<Summary>
+        /// Province 省
+        ///</Summary>
+        [Display(Name = "省")]
+        public string Province { get; set; }
+
+        ///<Summary>
+        /// City 市
+        ///</Summary>
+        [Display(Name = "市")]
+        public string City { get; set; }
+
+        ///<Summary>
+        /// County 县
+        ///</Summary>
+        [Display(Name = "县")]
+        public string County { get; set; }
+
+        ///<Summary>
+        /// Address 联系地址
+        ///</Summary>
+        [Display(Name = "联系地址")]
+        public string Address { get; set; }
+
+        ///<Summary>
+        /// QQ QQ
+        ///</Summary>
+        [Display(Name = "QQ")]
+        public string QQ { get; set; }
+
+        ///<Summary>
+        /// WeChat 微信
+        ///</Summary>
+        [Display(Name = "微信")]
+        public string WeChat { get; set; }
+
+        ///<Summary>
+        /// DingTalk 钉钉
+        ///</Summary>
+        [Display(Name = "钉钉")]
+        public string DingTalk { get; set; }
+
+        ///<Summary>
+        /// Skype Skype
+        ///</Summary>
+        [Display(Name = "Skype")]
+        public string Skype { get; set; }
+
+        ///<Summary>
+        /// WhatsApp WhatsApp
+        ///</Summary>
+        [Display(Name = "WhatsApp")]
+        public string WhatsApp { get; set; }
+
+        ///<Summary>
+        /// Memo Memo
+        ///</Summary>
+        [Display(Name = "Memo")]
+        public string Memo { get; set; }
     }
+}
 }
